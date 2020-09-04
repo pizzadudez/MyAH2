02971ca4f2412e2cba780afc9ccac8c2ca7aa86d
pizzadudez <vleadalbert@gmail.com>
2020-09-05 00:21:36 +0300

    frontend test

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return <div>Hello world</div>;
+  const [data, setData] = useState(null);
+  console.log(data);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:1999');
+      setData(response.data);
+    };
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>realm</th>
+            <th>GI Price</th>
+            <th>GIB Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(data).map(realmName => (
+            <tr key={realmName}>
+              <td>{realmName}</td>
+              <td>
+                {Math.round(data[realmName][72092].mean_price / 100) / 100}
+              </td>
+              <td>
+                {Math.round(data[realmName][72096].mean_price / 100) / 100}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const auctionsController = require('./controllers/auctions');
 
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json());
 
 app.get('/', auctionsController.get);
 
